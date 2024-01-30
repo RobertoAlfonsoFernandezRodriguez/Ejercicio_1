25c66704ff1c8ad2b728bb66cde224e0d364df26
RobertoAlfonsoFernandezRodriguez <202207255@mondragonmexico.edu.mx>
2024-01-30 14:50:06 -0600

    agregar

---
+++ Projecto2.X/main.c
@@ -0,0 +1,15 @@
+/*
+ * File:   main.c
+ * Author: Roberto Fernandez
+ *
+ * Created on 30 de enero de 2024, 02:28 PM
+ */
+
+
+#include <xc.h>
+#include "config.h"
+
+
+void main(void) {
+    return;
+}
